43dac1497f65ab046508f0c2f77a47b331014bb5
Kinal <kinjal.lalani@icreativetechnolabs.com>
2020-12-15 14:14:05 +0530

    [Init]Initial commit

---
+++ sale_dashboard/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "sale_dashboard",
+    'summary': """This module contains dashboard that shows details of sale""",
+    'description': """This module contains dashboard that shows details of sale""",
+    'author': "Aktiv software",
+    'website': "http://www.aktivsoftware.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
